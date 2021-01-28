a656c72c3b6e3c6a24ddef9f3e86e5508535ed2d
cqranjian <cqranjian@qq.com>
2021-01-28 16:06:27 +0800

    Update dc1.lua

--- luasrc/controller/dc1.lua
+++ luasrc/controller/dc1.lua
@@ -1,16 +1,3 @@
---[[
-
-Luci diag - Diagnostics controller module
-(c) 2009 Daniel Dickinson
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-        http://www.apache.org/licenses/LICENSE-2.0
-
-]]--
-
 module("luci.controller.dc1", package.seeall)
 
 function index()
@@ -21,7 +8,7 @@ function index()
 --entry({"admin","services","dc1"},alias("admin","services","dc1","base"),_("斐讯插排"),9).dependent=true
 entry({"admin", "services", "dc1"}, cbi("dc1/dc1"), _("斐讯插排")).dependent = true
 entry({"admin","services","dc1","refresh"},call("refresh_data"))
-entry({"admin","services","dc1","run"},call("act_status")).leaf=true
+entry({"admin","services","dc1","status"},call("act_status")).leaf=true
 end
 
 function act_status()
